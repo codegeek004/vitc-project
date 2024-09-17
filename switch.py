9417dafdf03914d11d1cb791b1483bbd35256991
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-09-17 12:49:15 +0530

    doorbell function

--- switch.py
+++ switch.py
@@ -6,12 +6,14 @@ while(1):
     b1 = sw1.value()
     if b1 == 0:
         led2.value(1)
-        print("Button pressed")
-        print(b1)
+        print("Button pressed", not b1)
+        
     else:
         led2.value(0)
+        print("button not pressed")
     #another approach
     #led2.value(not b1)
     time.sleep(0.5)
     
+
 
